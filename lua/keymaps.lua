a80423c8c6d11c11aef9679f8d17b785ea66f4e3
shurizzle <shura1991@gmail.com>
2021-12-25 00:10:48 +0100

    Add additional keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,3 +29,29 @@ for _, key in pairs({ 'Left', 'Right', 'Up', 'Down', 'PageUp', 'PageDown', 'End'
     opts
   )
 end
+
+keymap('', '<C-n>', '<cmd>tabn<CR>', {})
+keymap('', '<C-p>', '<cmd>tabp<CR>', {})
+
+-- Reselect visual selection after indenting
+keymap('v', '<', '<gv', { noremap = true })
+keymap('v', '>', '>gv', { noremap = true })
+
+-- Maintain the cursor position when yanking a visual selection
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+keymap('v', 'y', 'myy`y', { noremap = true })
+keymap('v', 'Y', 'myY`y', { noremap = true })
+
+-- Paste replace visual selection without copying it
+keymap('v', '<leader>p', '"_dP', { noremap = true })
+
+-- Make Y behave like the other capitals
+keymap('n', 'Y', 'y$', { noremap = true })
+
+vim.cmd([[
+function! ExecuteMacroOverVisualRange()
+  echo "@".getcmdline()
+  execute ":'<,'>normal @".nr2char(getchar())
+endfunction
+xnoremap @ :<C-u>call ExecuteMacroOverVisualRange()<CR>
+]])
