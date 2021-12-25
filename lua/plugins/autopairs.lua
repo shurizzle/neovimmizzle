09b6f41c2fffe3510112e9dcc4ef84be1d09501f
shurizzle <shura1991@gmail.com>
2021-12-25 20:35:39 +0100

    Configure autopairing angular brackets in Rust

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,17 @@
 local M = {}
 
 function M.config()
-  require('nvim-autopairs').setup()
+  local npairs = require('nvim-autopairs')
+  local Rule = require('nvim-autopairs.rule')
+
+  npairs.setup()
+
+  -- Not the best but I need angular brackets in Rust
+  npairs.add_rule(
+    Rule('<', '>', 'rust'):with_pair(
+      require('nvim-autopairs.conds').is_bracket_line()
+    )
+  )
 end
 
 return M
