4b9b4e9871b13999fdae0c269809cc9de030898b
shurizzle <shura1991@gmail.com>
2022-01-04 00:19:42 +0100

    Better configuration per indent-blankline

--- lua/config/plugins/indent-blankline.lua
+++ lua/config/plugins/indent-blankline.lua
@@ -1,21 +1,23 @@
 local M = {}
 
 function M.setup()
-  for k, v in pairs({
-    show_current_context = true,
-    show_current_context_start = true,
-    filetype_exclude = {
-      'dashboard',
-      'NvimTree',
-      'help',
-      'packer',
-      'lsp-installer',
-      'rfc',
-    },
-    buftype_exclude = {
-      'terminal',
-    },
-  }) do
+  for k, v in
+    pairs({
+      show_current_context = true,
+      show_current_context_start = true,
+      filetype_exclude = {
+        'dashboard',
+        'NvimTree',
+        'help',
+        'packer',
+        'lsp-installer',
+        'rfc',
+      },
+      buftype_exclude = {
+        'terminal',
+      },
+    })
+  do
     vim.g['indent_blankline_' .. k] = v
   end
 end
