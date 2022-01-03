5574cac3295236132bad808a93769cf68913eebc
shurizzle <shura1991@gmail.com>
2022-01-04 00:19:42 +0100

    Better configuration per indent-blankline

--- lua/config/plugins/indent-blankline.lua
+++ lua/config/plugins/indent-blankline.lua
@@ -1,20 +1,20 @@
 local M = {}
 
 function M.pre()
-  vim.g.indent_blankline_filetype_exclude = {
-    'dashboard',
-    'NvimTree',
-    'help',
-    'packer',
-    'lsp-installer',
-    'rfc',
-  }
-end
-
-function M.config()
   require('indent_blankline').setup({
     show_current_context = true,
     show_current_context_start = true,
+    filetype_exclude = {
+      'dashboard',
+      'NvimTree',
+      'help',
+      'packer',
+      'lsp-installer',
+      'rfc',
+    },
+    buftype_exclude = {
+      'terminal',
+    },
   })
 end
 
