b1a37d91c6fcb34f3e8b63d7bf14258cbd573d41
shurizzle <me@shurizzle.dev>
2022-06-22 21:47:26 +0200

    Add clippy to rust_analyzer

---
+++ lua/config/plugins/lsp/settings/rust_analyzer.lua
@@ -0,0 +1,17 @@
+return {
+  settings = {
+    ['rust-analyzer'] = {
+      checkOnSave = {
+        allFeatures = true,
+        overrideCommand = {
+          'cargo',
+          'clippy',
+          '--workspace',
+          '--message-format=json',
+          '--all-targets',
+          '--all-features',
+        },
+      },
+    },
+  },
+}
