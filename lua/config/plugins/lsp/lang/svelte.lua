f657aaa0240a833da1e190ba0470e037e94fc735
shurizzle <me@shurizzle.dev>
2022-09-29 23:38:40 +0200

    Add svelte lsp

---
+++ lua/config/plugins/lsp/lang/svelte.lua
@@ -0,0 +1,70 @@
+local _M = {}
+
+local util = require('config.plugins.lsp.util')
+local lsp = require('lspconfig')
+
+local function svelte(cb)
+  util.install_upgrade('svelte-language-server', function(ok)
+    if ok then
+      lsp.tsserver.setup({
+        on_attach = function(client, _)
+          if client.resolved_capabilities then
+            client.resolved_capabilities.document_formatting = false
+            client.resolved_capabilities.document_range_formatting = false
+          end
+
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+      })
+    end
+    cb()
+  end)
+end
+
+local function eslint(cb)
+  util.install_upgrade('eslint-lsp', function(ok)
+    if ok then
+      lsp.eslint.setup({
+        on_attach = function(client, _)
+          if client.resolved_capabilities then
+            client.resolved_capabilities.document_formatting = false
+            client.resolved_capabilities.document_range_formatting = false
+          end
+
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+        settings = {
+          format = false,
+        },
+      })
+    end
+    cb()
+  end)
+end
+
+local function prettier(cb)
+  util.install_upgrade('prettierd', function(ok)
+    if ok then
+      local null_ls = require('null-ls')
+      null_ls.register(null_ls.builtins.formatting.prettierd.with({
+        extra_filetypes = { 'svelte' },
+      }))
+    end
+    cb()
+  end)
+end
+
+function _M.config(cb)
+  svelte(function()
+    eslint(function()
+      prettier(function()
+        util.packer_load('inlay-hints.nvim')
+        cb()
+      end)
+    end)
+  end)
+end
+
+return _M
