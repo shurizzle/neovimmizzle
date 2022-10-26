0f61fbbe52501bf5ac8cc873f71984eec58aab5c
shurizzle <shura1991@gmail.com>
2022-10-26 06:56:39 +0200

    Enable osc52 only in ssh session

--- lua/config/plugins/osc52.lua
+++ lua/config/plugins/osc52.lua
@@ -1,19 +1,21 @@
 local M = {}
 
 function M.config()
-  local function copy(lines, _)
-    require('osc52').copy(table.concat(lines, '\n'))
-  end
+  if not is_ssh() then
+    local function copy(lines, _)
+      require('osc52').copy(table.concat(lines, '\n'))
+    end
 
-  local function paste()
-    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
-  end
+    local function paste()
+      return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+    end
 
-  vim.g.clipboard = {
-    name = 'osc52',
-    copy = { ['+'] = copy, ['*'] = copy },
-    paste = { ['+'] = paste, ['*'] = paste },
-  }
+    vim.g.clipboard = {
+      name = 'osc52',
+      copy = { ['+'] = copy, ['*'] = copy },
+      paste = { ['+'] = paste, ['*'] = paste },
+    }
+  end
 end
 
 return M
