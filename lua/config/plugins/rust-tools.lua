9d10eebcd3a9fa4327f152ad62c2f89daed6098d
shurizzle <me@shurizzle.dev>
2022-10-30 05:35:58 +0100

    Automatic dap setup for Rust

--- lua/config/plugins/rust-tools.lua
+++ lua/config/plugins/rust-tools.lua
@@ -1,6 +1,39 @@
 local _M = {}
 
 function _M.config()
+  local codelldb = vim.fn.exepath('codelldb')
+
+  if codelldb == '' then
+    ---@diagnostic disable-next-line
+    codelldb = nil
+  elseif codelldb then
+    local ok
+    ok, codelldb = pcall(vim.loop.fs_realpath, codelldb)
+    if not ok then
+      ---@diagnostic disable-next-line
+      codelldb = nil
+    end
+  end
+
+  local adapter = nil
+  if codelldb then
+    local libext = 'so'
+    if jit.os == 'Windows' then
+      libext = 'lib'
+    elseif jit.os == 'OSX' then
+      libext = 'dylib'
+    end
+
+    local codelib = join_paths(
+      vim.fn.fnamemodify(codelldb, ':h'),
+      'extension',
+      'lldb',
+      'lib',
+      'liblldb.' .. libext
+    )
+    adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb, codelib)
+  end
+
   require('rust-tools').setup({
     tools = {
       inlay_hints = {
@@ -32,6 +65,7 @@ function _M.config()
         },
       },
     },
+    dap = { adapter = adapter },
   })
 end
 
