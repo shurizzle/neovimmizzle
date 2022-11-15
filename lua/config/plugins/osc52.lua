727a19d2d33a64e9e4137542cf56cc392a04a6f1
shurizzle <shura1991@gmail.com>
2022-11-15 20:44:44 +0100

    Lazy load osc52

--- lua/config/plugins/osc52.lua
+++ lua/config/plugins/osc52.lua
@@ -1,21 +1,23 @@
-local M = {}
+local _M = {}
 
-function M.config()
-  if is_ssh() then
-    local function copy(lines, _)
-      require('osc52').copy(table.concat(lines, '\n'))
-    end
-
-    local function paste()
-      return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
-    end
-
-    vim.g.clipboard = {
-      name = 'osc52',
-      copy = { ['+'] = copy, ['*'] = copy },
-      paste = { ['+'] = paste, ['*'] = paste },
-    }
-  end
+function _M.cond()
+  return is_ssh()
 end
 
-return M
+function _M.config()
+  local function copy(lines, _)
+    require('osc52').copy(table.concat(lines, '\n'))
+  end
+
+  local function paste()
+    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+  end
+
+  vim.g.clipboard = {
+    name = 'osc52',
+    copy = { ['+'] = copy, ['*'] = copy },
+    paste = { ['+'] = paste, ['*'] = paste },
+  }
+end
+
+return _M
