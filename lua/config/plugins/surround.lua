cdcf233101c55fe7e61d75205dfc11fa6c498990
shurizzle <me@shurizzle.dev>
2022-11-16 06:49:53 +0100

    Lazy load nvim-surround

--- lua/config/plugins/surround.lua
+++ lua/config/plugins/surround.lua
@@ -1,7 +1,21 @@
 local _M = {}
 
+_M.keys = {
+  { 'n', 'ys' },
+  { 'n', 'yss' },
+  { 'n', 'yS' },
+  { 'n', 'ySS' },
+  { 'x', 'S' },
+  { 'x', 'gS' },
+  { 'n', 'ds' },
+  { 'n', 'cs' },
+}
+
 function _M.config()
-  require('nvim-surround.config').default_opts.aliases = {}
+  local opts = require('nvim-surround.config').default_opts
+  opts.keymaps.insert = nil
+  opts.keymaps.insert_line = nil
+  opts.aliases = {}
   require('nvim-surround').setup()
 end
 
