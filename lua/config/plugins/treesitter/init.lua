c3ec52a1cee44f9f8333ead2153b4e45464bae26
shurizzle <me@shurizzle.dev>
2022-11-19 11:43:08 +0100

    Improve treesitter configuration

--- lua/config/plugins/treesitter/init.lua
+++ lua/config/plugins/treesitter/init.lua
@@ -22,9 +22,18 @@ function _M.config()
 
   vim.o.foldmethod = 'expr'
   vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-  vim.api.nvim_create_autocmd({ 'BufReadPost', 'FileReadPost' }, {
-    command = 'normal zR',
+  vim.api.nvim_create_autocmd({ 'BufReadPost', 'FileReadPost', 'FileType' }, {
+    callback = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.schedule(function()
+        vim.api.nvim_buf_call(bufnr, function() vim.cmd.normal('zR') end)
+      end)
+    end,
   })
+
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    vim.api.nvim_buf_call(bufnr, function() vim.cmd.normal('zR') end)
+  end
 end
 
 return _M
