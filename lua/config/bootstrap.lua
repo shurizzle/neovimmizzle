b198fabbb53cd4b341b7b2f95f085a6df8d41c40
shurizzle <shura1991@gmail.com>
2022-11-19 02:39:26 +0100

    Improve bootstrap

--- lua/config/bootstrap.lua
+++ lua/config/bootstrap.lua
@@ -3,30 +3,44 @@ local function git_clone(url, dir, callback)
     join_paths(vim.fn.stdpath('data'), 'site', 'pack', 'packer', 'start', dir)
 
   if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    callback(vim.fn.system({
+    vim.fn.system({
       'git',
       'clone',
       '--depth',
       '1',
       url,
       install_path,
-    }))
+    })
+    callback(vim.v.shell_error)
   end
 end
 
 git_clone(
   'https://github.com/lewis6991/impatient.nvim',
   'impatient.nvim',
-  function(_) vim.cmd([[packadd impatient.nvim]]) end
+  function(res)
+    if res == 0 then vim.cmd([[packadd impatient.nvim]]) end
+  end
 )
 
-require('impatient')
+xpcall(
+  function() require('impatient') end,
+  function(_)
+    vim.api.nvim_echo(
+      { { 'Error while loading impatient', 'ErrorMsg' } },
+      true,
+      {}
+    )
+  end
+)
 
 git_clone(
   'https://github.com/wbthomason/packer.nvim',
   'packer.nvim',
   function(res)
-    vim.g.packer_bootstrap = res
-    vim.cmd([[packadd packer.nvim]])
+    if res == 0 then
+      vim.g.packer_bootstrap = true
+      vim.cmd([[packadd packer.nvim]])
+    end
   end
 )
