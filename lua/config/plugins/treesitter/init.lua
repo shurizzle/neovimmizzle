7f66a95d39e49ed70612b53f88c9d47f75c99e8f
shurizzle <shura1991@gmail.com>
2022-11-20 04:29:40 +0100

    Remove useless code

--- lua/config/plugins/treesitter/init.lua
+++ lua/config/plugins/treesitter/init.lua
@@ -20,20 +20,9 @@ function _M.config()
     },
   })
 
+  vim.o.foldenable = false
   vim.o.foldmethod = 'expr'
   vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-  vim.api.nvim_create_autocmd({ 'BufReadPost', 'FileReadPost', 'FileType' }, {
-    callback = function()
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.schedule(function()
-        vim.api.nvim_buf_call(bufnr, function() vim.cmd.normal('zR') end)
-      end)
-    end,
-  })
-
-  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    vim.api.nvim_buf_call(bufnr, function() vim.cmd.normal('zR') end)
-  end
 end
 
 return _M
