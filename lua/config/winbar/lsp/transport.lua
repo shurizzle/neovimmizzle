54fb4cc4d31150d6bf4bb5f644727c76a9c91cd3
shurizzle <me@shurizzle.dev>
2022-11-21 18:46:53 +0100

    Support SymbolInformation for textDocument/documentSymbol request

--- lua/config/winbar/lsp/transport.lua
+++ lua/config/winbar/lsp/transport.lua
@@ -35,7 +35,7 @@ local _M = {}
 
 ---@class lsp.Location
 ---@field uri lsp.DocumentUri
----@field range Range
+---@field range lsp.Range
 
 ---@class lsp.DocumentSymbol
 ---@field name string
@@ -104,8 +104,130 @@ local s = require('config.winbar.lsp.state')
 
 local METHOD = 'textDocument/documentSymbol'
 
+---@alias config.winbar.lsp.transport.SymbolOrdering
+---| 'after'
+---| 'before'
+---| 'around'
+---| 'within'
+
+---@param node1 lsp.DocumentSymbol
+---@param node2 lsp.DocumentSymbol
+---@return config.winbar.lsp.transport.SymbolOrdering
+local function symbol_compare(node1, node2)
+  local r1 = node1.selectionRange
+  local r2 = node2.selectionRange
+
+  if
+    r2['end'].line < r1.start.line
+    or (
+      r2['end'].line == r1.start.line
+      and r2['end'].character <= r1.start.character
+    )
+  then
+    return 'before'
+  end
+
+  if
+    r2.start.line > r1['end'].line
+    or (
+      r2.start.line == r1['end'].line
+      and r2.start.character >= r1['end'].character
+    )
+  then
+    return 'after'
+  end
+
+  if
+    (
+      r2.start.line < r1.start.line
+      or (
+        r2.start.line == r1.start.line
+        and r2.start.character <= r1.start.character
+      )
+    )
+    and (
+      r2['end'].line > r1['end'].line
+      or (
+        r2['end'].line == r1['end'].line
+        and r2['end'].character >= r1['end'].character
+      )
+    )
+  then
+    return 'around'
+  end
+
+  return 'within'
+end
+
+---@param tree lsp.DocumentSymbol[]
+---@param node lsp.DocumentSymbol
+local function insert(tree, node)
+  if #tree == 0 then return table.insert(tree, node) end
+  ---@type lsp.DocumentSymbol[]|nil
+  local root = tree
+  while root do
+    (function()
+      ---@type integer
+      local last = 1
+
+      for i, n in ipairs(root) do
+        local order = symbol_compare(n, node)
+
+        if order == 'within' then
+          if not n.children then n.children = {} end
+          root = n.children
+          return
+        elseif order == 'around' then
+          if not node.children then node.children = {} end
+          insert(node.children, table.remove(root, i))
+          return
+        elseif order == 'after' then
+          last = i
+        elseif order == 'before' then
+          table.insert(root, i, node)
+          return
+        end
+      end
+
+      table.insert(root, last + 1, node)
+      root = nil
+    end)()
+  end
+
+  -- TODO
+end
+
+---@param data lsp.DocumentSymbol[]|lsp.SymbolInformation[]|nil
+---@return lsp.DocumentSymbol[]|nil
+local function transform(data)
+  if not data then return end
+  if #data == 0 then return data end
+  if data[1].range then return data end
+
+  ---@type lsp.DocumentSymbol[]
+  local tree = {}
+
+  for _, info in
+    ipairs(data --[[ @as lsp.SymbolInformation[] ]])
+  do
+    ---@type lsp.DocumentSymbol
+    insert(tree, {
+      name = info.name,
+      detail = nil,
+      kind = info.kind,
+      tags = info.tags,
+      deprecated = info.deprecated,
+      range = info.location.range,
+      selectionRange = info.location.range,
+      children = nil,
+    })
+  end
+
+  return tree
+end
+
 ---@param bufnr integer
----@param handler fun(err: lsp.ResponseError, data: lsp.SymbolInformation[], info: vim.lsp.HandlerInfo)
+---@param handler fun(err: lsp.ResponseError|nil, data: lsp.DocumentSymbol[]|nil, info: vim.lsp.HandlerInfo)
 ---@return boolean
 ---@return integer|nil
 function _M.request(bufnr, handler)
@@ -120,7 +242,12 @@ function _M.request(bufnr, handler)
   local params = {
     textDocument = vim.lsp.util.make_text_document_params(bufnr),
   }
-  return client.request(METHOD, params, handler, bufnr)
+  return client.request(
+    METHOD,
+    params,
+    function(err, data, info) return handler(err, transform(data), info) end,
+    bufnr
+  )
 end
 
 return _M
