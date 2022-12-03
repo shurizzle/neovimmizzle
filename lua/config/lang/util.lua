2986e943fc0cf009a4ec97a456744d26b9bc7ac0
shurizzle <shura1991@gmail.com>
2022-12-03 08:47:30 +0100

    Fix notification update

--- lua/config/lang/util.lua
+++ lua/config/lang/util.lua
@@ -1,5 +1,7 @@
 local _M = {}
 
+local Future = require('config.future')
+
 function _M.packer_load(what)
   if not packer_plugins[what] or not packer_plugins[what].loaded then
     require('packer').loader(what)
@@ -15,30 +17,41 @@ local function default_timeout()
 end
 
 function _M.notify_progress(init, finally)
-  local id = nil
+  return Future.new(function(resolve, reject)
+    vim.schedule(function()
+      local id = nil
+      local win = vim.api.nvim_get_current_win()
 
-  local function notify_new(msg, log, opts)
-    log = log or vim.log.levels.INFO
-    opts = opts or {}
-    opts.timeout = false
+      local function notify_new(msg, log, opts)
+        log = log or vim.log.levels.INFO
+        opts = opts or {}
+        opts.timeout = false
 
-    id = vim.notify(msg, log, opts)
-    return vim.deepcopy(id)
-  end
+        id = vim.notify(msg, log, opts)
+        return vim.deepcopy(id)
+      end
 
-  local function notify_edit(msg, log, opts)
-    log = log or vim.log.levels.ERROR
-    opts = opts or {}
-    opts.replace = vim.deepcopy(id)
-    opts.timeout = opts.timeout or default_timeout()
-    return vim.notify(msg, log, opts)
-  end
+      local function notify_edit(msg, log, opts)
+        log = log or vim.log.levels.ERROR
+        opts = opts or {}
+        opts.replace = vim.deepcopy(id)
+        opts.timeout = opts.timeout or default_timeout()
 
-  local future = init(notify_new)
+        local ok, res = pcall(
+          vim.api.nvim_win_call,
+          win,
+          function() return vim.notify(msg, log, opts) end
+        )
+        if not ok then res = vim.notify(msg, log, opts) end
 
-  future:finally(function(...) finally(notify_edit, ...) end)
+        return res
+      end
 
-  return future
+      local future = init(notify_new)
+      future:finally(function(...) finally(notify_edit, ...) end)
+      future:and_then(resolve, reject)
+    end)
+  end)
 end
 
 return _M
