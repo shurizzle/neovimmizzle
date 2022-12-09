27bdf8a962fa8b4082fde2ea6bf1efa8259d2e1a
shurizzle <shura1991@gmail.com>
2022-12-10 00:55:00 +0100

    feat(lang.haskell): add fourmolu

--- lua/config/lang/_/haskell.lua
+++ lua/config/lang/_/haskell.lua
@@ -11,7 +11,13 @@ function _M.config()
   return Future.join({
     require('config.lang.lsp').hls,
     require('config.lang.formatters').fourmolu,
-  })
+  }):and_then(function(res)
+    if res[1][1] then
+      return Future.resolved(res[1][2])
+    else
+      return Future.rejected(res[1][2])
+    end
+  end)
 end
 
 return _M
--- lua/config/lang/formatters/fourmolu.lua
+++ lua/config/lang/formatters/fourmolu.lua
@@ -1,4 +1,4 @@
-return require('config.future').pcall(function()
+return require('config.lang.installer').fourmolu:and_then(function()
   local null_ls = require('null-ls')
   null_ls.register(null_ls.builtins.formatting.fourmolu)
 end)
