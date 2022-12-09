d95a8e030056b48f4fe982f12c53ae429e92bbaf
shurizzle <shura1991@gmail.com>
2022-12-09 08:27:14 +0100

    Add fourmolu formatter to haskell config

--- lua/config/lang/_/haskell.lua
+++ lua/config/lang/_/haskell.lua
@@ -1,10 +1,17 @@
 local _M = {}
 
+local Future = require('config.future')
+
 _M.filetypes = {
   'haskell',
   'lhaskell',
 }
 
-function _M.config() return require('config.lang.lsp').hls end
+function _M.config()
+  return Future.join({
+    require('config.lang.lsp').hls,
+    require('config.lang.formatters').fourmolu,
+  })
+end
 
 return _M
