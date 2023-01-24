3d733de4f626948bb970f1a5fe355643c3739f44
shurizzle <me@shurizzle.dev>
2023-01-24 18:08:06 +0100

    Fixing code style

--- lua/config/lang/_/php.lua
+++ lua/config/lang/_/php.lua
@@ -2,7 +2,7 @@ local _M = {}
 
 local Future = require('config.future')
 
-function _M.config(cb)
+function _M.config()
   return Future.join({
     require('config.lang.lsp').intelephense,
     require('config.lang.formatters')['php-cs-fixer'],
