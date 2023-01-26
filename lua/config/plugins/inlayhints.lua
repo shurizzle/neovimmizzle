54379fb74992a4e87ca975cad6a08c0eb0f5d028
shurizzle <shura1991@gmail.com>
2023-01-26 18:02:31 +0100

    Lazy load inlayhints

--- lua/config/plugins/inlayhints.lua
+++ lua/config/plugins/inlayhints.lua
@@ -1,5 +1,12 @@
 local _M = {}
 
+_M.module_pattern = {
+  '^lsp%-inlayhints$',
+  '^lsp%-inlayhints%.',
+}
+
+_M.opt = true
+
 function _M.config() require('lsp-inlayhints').setup({}) end
 
 return _M
