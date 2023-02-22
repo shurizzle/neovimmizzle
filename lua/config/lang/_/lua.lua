0d072ea8172257a31e5cdf8004b35c584d7debdd
shurizzle <me@shurizzle.dev>
2023-02-22 11:54:14 +0100

    Using lua_ls instead of deprecated sumneko_lua

--- lua/config/lang/_/lua.lua
+++ lua/config/lang/_/lua.lua
@@ -4,7 +4,7 @@ local Future = require('config.future')
 
 function _M.config()
   return Future.join({
-    require('config.lang.lsp').sumneko_lua,
+    require('config.lang.lsp').lua_ls,
     require('config.lang.formatters').stylua,
   }):and_then(function(res)
     if not res[1][1] then return Future.rejected(res[1][2]) end
--- lua/config/lang/lsp/lua_ls.lua
+++ lua/config/lang/lsp/lua_ls.lua
@@ -2,7 +2,7 @@ return require('config.lang.installer')['lua-language-server']:and_then(
   function()
     require('config.lang.util').packer_load('neodev.nvim')
 
-    require('lspconfig').sumneko_lua.setup({
+    require('lspconfig').lua_ls.setup({
       on_attach = function(client, _)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
