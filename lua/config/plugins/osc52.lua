9034710494f4781903d9cc5349f0f789ab5af18c
shurizzle <me@shurizzle.dev>
2023-03-14 03:56:16 +0100

    Fix osc52 paste

--- lua/config/plugins/osc52.lua
+++ lua/config/plugins/osc52.lua
@@ -3,10 +3,20 @@ local _M = {}
 function _M.cond() return require('config.platform').is.ssh end
 
 function _M.config()
+  require('osc52').setup({
+    max_length = 0,
+    silent = false,
+    trim = false,
+  })
+
   local function copy(lines, _) require('osc52').copy(table.concat(lines, '\n')) end
 
   local function paste()
-    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+    local lines = {}
+    for line in vim.gsplit(vim.fn.getreg(''), '\n', true) do
+      table.insert(lines, line)
+    end
+    return { lines, vim.fn.getregtype('') }
   end
 
   vim.g.clipboard = {
