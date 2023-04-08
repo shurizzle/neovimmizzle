c5f8d0a61311e88816ad0b57762a71e7179cbee8
shurizzle <me@shurizzle.dev>
2023-04-09 00:42:56 +0200

    Update haskell support

--- lua/config/lang/_/haskell.lua
+++ lua/config/lang/_/haskell.lua
@@ -13,19 +13,22 @@ _M.filetypes = {
 -- cabal update
 -- ghcup compile hls -g 1.8.0.0 --ghc 9.2.5 -- -f 'cabal cabalfmt class callHierarchy eval importLens refineImports rename retrie hlint moduleName pragmas qualifyImportedNames codeRange changeTypeSignature explicitFixity explicitFields fourmolu refactor'
 -- ghcup compile hls -g 1.9.0.0 --ghc 9.2.5 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicit Fixity explicitFields fourmolu refactor dynamic cabalfmt'
+-- ghcup compile hls -g 1.10.0.0 --ghc 9.2.7 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicitFixity explicitFields fourmolu refactor dynamic cabalfmt'
 
 function _M.config()
-  return Future.pcall(function()
-    require('haskell-tools').setup({
-      hls = {
-        filetypes = {
-          'haskell',
-          'lhaskell',
-          'cabal',
+  return Future.pcall(
+    function()
+      require('haskell-tools').setup({
+        hls = {
+          filetypes = {
+            'haskell',
+            'lhaskell',
+            'cabal',
+          },
         },
-      },
-    })
-  end)
+      })
+    end
+  )
 end
 
 return _M
