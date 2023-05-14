dd64f218803989c8146b56c9959a26d51b10c195
shurizzle <me@shurizzle.dev>
2023-05-14 18:44:37 +0200

    Remove cmdline support in cmp

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -177,15 +177,6 @@ function _M.config()
     }),
   })
 
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' },
-    }, {
-      { name = 'cmdline' },
-    }),
-  })
-
   cmp.setup.cmdline({ '/', '?' }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
