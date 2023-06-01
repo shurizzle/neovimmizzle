ee1b7cc171d31a66f66bcfa9b705c5e9245d3cfd
shurizzle <me@shurizzle.dev>
2023-06-01 10:26:45 +0200

    Update nlsp-settings config

--- lua/config/plugins/nlsp-settings.lua
+++ lua/config/plugins/nlsp-settings.lua
@@ -8,7 +8,7 @@ function _M.config()
   nlsp.setup({
     config_home = vim.fn.stdpath('config') .. '/nlsp-settings',
     local_settings_dir = '.nlsp-settings',
-    append_default_schemas = false,
+    append_default_schemas = true,
     loader = 'json',
   })
 end
