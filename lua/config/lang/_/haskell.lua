a386ae8ab7432296015d203107a83e1a6f2d3c56
shurizzle <shura1991@gmail.com>
2023-06-01 10:20:58 +0200

    Update hints for haskell-language-server

--- lua/config/lang/_/haskell.lua
+++ lua/config/lang/_/haskell.lua
@@ -9,11 +9,12 @@ _M.filetypes = {
   'cabal',
 }
 
--- HINT:
--- cabal update
+-- NOTE:
+--! cabal update
 -- ghcup compile hls -g 1.8.0.0 --ghc 9.2.5 -- -f 'cabal cabalfmt class callHierarchy eval importLens refineImports rename retrie hlint moduleName pragmas qualifyImportedNames codeRange changeTypeSignature explicitFixity explicitFields fourmolu refactor'
 -- ghcup compile hls -g 1.9.0.0 --ghc 9.2.5 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicit Fixity explicitFields fourmolu refactor dynamic cabalfmt'
 -- ghcup compile hls -g 1.10.0.0 --ghc 9.2.7 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicitFixity explicitFields fourmolu refactor dynamic cabalfmt'
+-- ghcup compile hls -g 1.10.0.0 --ghc 9.2.8 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicitFixity explicitFields fourmolu refactor dynamic cabalfmt'
 
 function _M.config()
   return Future.pcall(
