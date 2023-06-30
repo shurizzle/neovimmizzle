f32347bd342873d76b4d00125b3b70d4b10023b8
shurizzle <shura1991@gmail.com>
2023-06-30 16:22:57 +0200

    Add doc for haskell-language-server 2.0.0.1

--- lua/config/lang/_/haskell.lua
+++ lua/config/lang/_/haskell.lua
@@ -15,6 +15,7 @@ _M.filetypes = {
 -- ghcup compile hls -g 1.9.0.0 --ghc 9.2.5 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicit Fixity explicitFields fourmolu refactor dynamic cabalfmt'
 -- ghcup compile hls -g 1.10.0.0 --ghc 9.2.7 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicitFixity explicitFields fourmolu refactor dynamic cabalfmt'
 -- ghcup compile hls -g 1.10.0.0 --ghc 9.2.8 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicitFixity explicitFields fourmolu refactor dynamic cabalfmt'
+-- ghcup compile hls -g 2.0.0.1 --ghc 9.2.8 -- -f 'cabal class callHierarchy haddockComments eval importLens refineImports rename retrie tactic hlint stan moduleName pragmas splice alternateNumberFormat qualifyImportedNames codeRange changeTypeSignature gadt explicitFixity explicitFields overloadedRecordDot fourmolu refactor dynamic cabalfmt'
 
 function _M.config()
   return Future.pcall(
