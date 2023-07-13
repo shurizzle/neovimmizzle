031333fe18dff6587a9b7b3103820f31631eb1bd
shurizzle <me@shurizzle.dev>
2023-07-13 02:48:44 +0200

    sh: Switching from shfmt to beautysh

--- lua/config/lang/_/sh.lua
+++ lua/config/lang/_/sh.lua
@@ -5,7 +5,7 @@ function _M.config()
 
   return Future.join({
     require('config.lang.linters').shellcheck,
-    require('config.lang.formatters').shfmt,
+    require('config.lang.formatters').beautysh,
   }):and_then(function(res)
     if not res[1][1] then return Future.rejected(res[1][2]) end
     if not res[2][1] then return Future.rejected(res[2][2]) end
