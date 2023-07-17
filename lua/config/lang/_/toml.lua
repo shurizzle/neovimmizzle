cf8987e8189b2f4bbb01788e637f8ed8fd09832e
shurizzle <shura1991@gmail.com>
2023-07-17 10:08:21 +0200

    Add fallback for taplo

--- lua/config/lang/_/toml.lua
+++ lua/config/lang/_/toml.lua
@@ -1,5 +1,9 @@
 local _M = {}
 
-function _M.config() return require('config.lang.lsp').taplo end
+function _M.config()
+  return require('config.lang.lsp').taplo:catch(
+    function() return require('config.lang.fallback')('taplo') end
+  )
+end
 
 return _M
